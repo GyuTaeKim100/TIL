19c6f8f4ffb951543c856f4918c732ec8d527e01
GyuTaeK kim <gyutae.kim@vpluslab.kr>
2022-04-19 17:20:08 +0900

    feat: Add morgan

--- backend_side_projects/node-js-toy-projects-2022/hello-express-world/index.js
+++ backend_side_projects/node-js-toy-projects-2022/hello-express-world/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const morgan = require('morgan');
+
 const app = express();
 
+// 로거 미들웨어
 function logger(req, res, next) {
-	console.log('i am logger');
+	// console.log('i am logger');
+	next();
+}
+
+// 로거2 미들웨어
+function logger2(req, res, next) {
+	// console.log('i am logger2');
+	next();
 }
 
 app.use(logger);
+app.use(logger2);
+app.use(morgan('dev'));
 
 app.listen(5000, function () {
 	console.log('server is running');
