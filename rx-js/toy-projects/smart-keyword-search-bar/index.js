4cb80b0fd636da2ae12fb41f59e1d26bc11b58f2
gyutaekim <gyutae100@gmail.com>
2023-01-05 22:06:31 +0900

    update

--- rx-js/toy-projects/smart-keyword-search-bar/index.js
+++ rx-js/toy-projects/smart-keyword-search-bar/index.js
@@ -1,12 +1,12 @@
-const {fromEvent, from} = rxjs
+const {fromEvent, from, merge} = rxjs
 const {ajax} = rxjs.ajax
-const {mergeMap, switchMap, pluck, retry, map, scan, filter, debounceTime, distinctUntilChanged} = rxjs.operators
+const {mergeMap, switchMap, pluck, retry, map,mapTo, scan, filter, debounceTime, distinctUntilChanged} = rxjs.operators
 
 const url = 'http://127.0.0.1:3000/people/quarter-error'
 const keyword = document.querySelector('#keyword')
 const result = document.querySelector('#result')
 
-fromEvent(keyword, 'keyup').pipe(
+const searchInit$ = fromEvent(keyword, 'keyup').pipe(
     // backspace 시, 요청 안함.
     filter(event => event.code != 'Backspace'),
     pluck('target', 'value'),
@@ -14,18 +14,28 @@ fromEvent(keyword, 'keyup').pipe(
     filter(typed => typed.length > 1), // 1글자 이상일 경우만
     debounceTime(500), // 0.5초 공백 후 발행
     distinctUntilChanged(), // 연속된 같은 문자열 생략
-    // mergeMap 대신, switchMap 사용
-    switchMap(keyword => 
-        ajax(`${url}?name=${keyword}`).pipe(retry(3))        
-    ),
-    pluck('response')
-).subscribe(showResults)
 
-function showResults (results) {
-    console.log('results',results)
-    from(results).pipe(
+)
+
+const searching$ = searchInit$.pipe(
+    mapTo(`<div class="searching">Searching ...</div>`)    
+)
+
+const searchResult$=  searchInit$.pipe( 
+    switchMap(keyword => 
+        ajax(`${url}?name=${keyword}`).pipe(retry(3))
+    ),
+    pluck('response'),
+    // mergeMap or switchMap
+    mergeMap(results =>  from(results).pipe(
         map(person => `${person.first_name} ${person.last_name}`),
         map(name => `<article> ${name} </article>`),
         scan((acc, article) => acc += article, '')
-    ).subscribe(people => result.innerHTML = people)
-}
+    ))
+)
+
+merge(
+    searching$,
+    searchResult$
+).subscribe(text => result.innerHTML = text)
+
