e614766d6f8f140e1684ceaa7aa1fbd98ab7023b
GyuTae Kim <gyutae.kim@vpluslab.kr>
2023-01-20 18:20:08 +0900

    삭제

--- test/toy-jest/unit-basic-challenge/src/basic/test/calculator.test.js
+++ test/toy-jest/unit-basic-challenge/src/basic/test/calculator.test.js
@@ -1,67 +1,31 @@
-const Calculator = require('../calculator.js');
+const fetchProduct = require('../async.js')
 
-describe('Calculator', () => {
-  let cal;
-  beforeEach(() => {
-    cal = new Calculator();
-  });
+describe('Async', () => {
+  // 5초 대기함. return 방식 추천
+  it('async - done', (done) => {
+    fetchProduct().then(item => {
+      expect(item).toEqual({item: 'Milk', price: 200})
+      done();
+    })
+  })
+  
+  it('async - return', () => {
+    return fetchProduct().then(item => {
+      expect(item).toEqual({item: 'Milk', price: 200})
+    })
+  })
 
-  it('inits with 0', () => {
-    expect(cal.value).toBe(0);
-  });
+  // 추천
+  it('async - await', async () => {
+    const product =  await fetchProduct()
+    expect(product).toEqual({item: 'Milk', price: 200})
+  })
 
-  it('sets', () => {
-    cal.set(9);
-    expect(cal.value).toBe(9);
-  });
+  it('async - resolves', () => {
+   return expect(fetchProduct()).resolves.toEqual({item: 'Milk', price: 200})
+  })
 
-  it('clear', () => {
-    cal.set(9);
-    cal.clear();
-    expect(cal.value).toBe(0);
-  });
-
-  it('adds', () => {
-    cal.set(1);
-    cal.add(2);
-
-    expect(cal.value).toBe(3);
-  });
-
-  it('add should throw an error if value is greater than 100', () => {
-    expect(() => {
-      cal.add(101);
-    }).toThrow('Value can not be greater than 100');
-  });
-
-  it('subtracts', () => {
-    cal.subtract(1);
-
-    expect(cal.value).toBe(-1);
-  });
-
-  it('multiplies', () => {
-    cal.set(5);
-    cal.multiply(4);
-
-    expect(cal.value).toBe(20);
-  });
-
-  describe('divides', () => {
-    it('0 / 0 === NaN', () => {
-      cal.divide(0);
-      expect(cal.value).toBe(NaN);
-    });
-    it('1 / 0 === Infinity', () => {
-      cal.set(1);
-      cal.divide(0);
-      expect(cal.value).toBe(Infinity);
-    });
-    it('4 / 4 === 1', () => {
-      cal.set(4);
-      cal.divide(4);
-
-      expect(cal.value).toBe(1);
-    });
-  });
-});
+  it('async - reject', () => {
+    return expect(fetchProduct('error')).resolves.rejects.toBe('network error')
+  })
+})
