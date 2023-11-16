505682cd85db296ee44b5e51c06d81795daa78b6
GyuTaeKim <gyutae100@gmail.com>
2023-11-16 21:02:56 +0900

    기획 초안 개선

--- ramda-js/side-projects/tree-ds-utils/index.test.ts
+++ ramda-js/side-projects/tree-ds-utils/index.test.ts
@@ -6,3 +6,42 @@ test('adds 1 + 2 to equal 3', () => {
 });
 
 const sum = (a, b) => a + b
+
+test('test ', ()=> {
+    const tree = [{
+          name: 'A',
+          children: [
+            {
+              name: 'B',
+              children: [
+                {name: 'C', children: []},
+                {name: 'D', children: []},
+              ],
+            },
+            {
+              name: 'E',
+              children: [{name: 'F', children: []}],
+            },
+          ],
+        }]; 
+
+        const tree2 = [{
+            name: 'A',
+            children: [
+              {
+                name: 'B',
+                children: [
+                  {name: 'C', children: []},
+                  {name: 'D', children: []},
+                ],
+              },
+              {
+                name: 'E-2',
+                children: [{name: 'F', children: []}],
+              },
+            ],
+          }]; 
+    
+        expect(tree).toMatchObject(tree2)
+
+})
