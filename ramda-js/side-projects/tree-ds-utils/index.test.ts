d1f5daf3da277977fe7b586bbdd765203f1e3df8
GyuTae Kim <gyutae.kim@vpluslab.kr>
2023-11-22 11:20:06 +0900

    test(deepFlatten function) : Add unit tests

--- ramda-js/side-projects/tree-ds-utils/index.test.ts
+++ ramda-js/side-projects/tree-ds-utils/index.test.ts
@@ -1,14 +1,14 @@
 
 import { expect, it, test} from '@jest/globals';
 
-import { hasChildren, isLeafNode  } from './index'
+import { hasChildren, isLeafNode, deepFlatten  } from './index'
 
 describe('hasChildren', ()=> {
 
   test('node의 자식이 배열이면서 길이가 1 이상인 경우, true를 반환한다.', ()=> {
     const childrenKey = 'children'
     const node = { [childrenKey]: [{name:'children 1-1'}] }
-
+    
     expect(hasChildren(childrenKey, node )).toEqual(true)
   })  
 
@@ -63,37 +63,69 @@ describe('isLeafNode', ()=> {
   })
 })
 
+describe('deepFlatten', ()=> {
+  test('다중 root node를 가진 배열의 트리 구조에 대해서, 모든 node를 전위 순회 순서의 1depth 배열 형식으로 반환한다.', ()=> {
+    const childrenKey = 'children'
 
-const tree = [{
-  name: 'A',
-  children: [
-    {
-      name: 'B',
-      children: [
-        {name: 'C', children: []},
-        {name: 'D', children: []},
-      ],
-    },
-    {
-      name: 'E',
-      children: [{name: 'F', children: []}],
-    },
-  ],
-}]; 
+    const nodes = [
+      {name: '1', children: [
+        {name: '1-1', children: [
+          {name: '1-1-1', children: [
+            {name: '1-1-1-1', children: []},
+            {name: '1-1-1-2', children: []},
+          ]},
+          ]},
+          {name: '1-1-2', children: []},
+        ]
+      },
+      {name: '1-2', children: [
+          {name: '1-2-1', children: [
+            {name: '1-2-1-1', children: []},
+            {name: '1-2-1-2', children: []},
+          ]},
+          {name: '1-2-2', children: []},
+        ]
+      },
+      {name: '1-3', children: []}, 
+    ]
 
-const tree2 = [{
-    name: 'A',
-    children: [
-      {
-        name: 'B',
-        children: [
-          {name: 'C', children: []},
-          {name: 'D', children: []},
-        ],
-      },
-      {
-        name: 'E-2',
-        children: [{name: 'F', children: []}],
-      },
-    ],
-  }]; 
+    expect(deepFlatten(childrenKey, nodes))
+    .toEqual([
+      expect.objectContaining({name: '1'}),
+      expect.objectContaining({name: '1-1'}),
+      expect.objectContaining({name: '1-1-1'}),
+      expect.objectContaining({name: '1-1-1-1'}),
+      expect.objectContaining({name: '1-1-1-2'}),
+      expect.objectContaining({name: '1-1-2'}),
+      expect.objectContaining({name: '1-2'}),
+      expect.objectContaining({name: '1-2-1'}), 
+      expect.objectContaining({name: '1-2-1-1'}),
+      expect.objectContaining({name: '1-2-1-2'}),
+      expect.objectContaining({name: '1-2-2'}),
+      expect.objectContaining({name: '1-3'}),  
+    ])
+  })
+
+  test('빈 배열인 경우, 빈 배열을 반환한다.', ()=> {
+    const childrenKey = 'children'
+    const nodes = []
+
+    expect(deepFlatten(childrenKey, nodes)).toEqual([])
+  })
+
+  test('배열 타입이 아닌 경우, error를 throw한다', () => {
+    const childrenKey = 'children'
+    const nodes = `Invalid node type`
+
+    expect(() => deepFlatten(childrenKey, nodes)).toThrow()
+  })
+
+  test('자식 prop이 존재하지 않는 경우, error를 throw한다.', () => {
+    const childrenKey = 'children'
+    const nodes = [{ }]
+
+    expect(() => deepFlatten(childrenKey, nodes)).toThrow()
+  })
+
+})
+
